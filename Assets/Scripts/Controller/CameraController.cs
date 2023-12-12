7d742a4cadadbabcfcb8f5f0a7b1d40c003bb254
Daishikofy <daishi.setsuko@gmail.com>
2023-12-12 18:52:28 +0100

    Hiding v0.1

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -6,17 +6,73 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform PlayerTransform;
-    private Vector3 _offset;
+    private enum CameraState
+    {
+        TopDown,
+        POV
+    }
 
+    public static CameraController Instance => _instance;
+    private static CameraController _instance;
+
+    [SerializeField] private Transform PlayerTransform;
+    [SerializeField] private Transform TopDownContainer;
+    [SerializeField] private Transform PovContrainer;
+
+    private Vector3 _offset;
+    
+    private CameraState _currentState = CameraState.TopDown;
 
     private void Start()
     {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
         _offset = transform.position - PlayerTransform.position;
+
+        switch (_currentState)
+        {
+              case CameraState.TopDown:
+                  transform.SetParent(TopDownContainer);
+                  break;
+              case CameraState.POV:
+                  transform.SetParent(PovContrainer);
+                  break;
+        }
     }
 
     private void LateUpdate()
     {
+        switch (_currentState)
+        {
+            case CameraState.TopDown:
+                UpdateTopDown();
+                break;
+            case CameraState.POV:
+                break;
+        }
+    }
+
+    private void UpdateTopDown()
+    {
         transform.position = PlayerTransform.position + _offset;
+    }
+
+    public void SetTopDownCamera()
+    {
+        _currentState = CameraState.TopDown;
+
+        transform.SetParent(TopDownContainer, false);
+    }
+
+    public void SetPovCamera()
+    {
+        _currentState = CameraState.POV;
+        
+        transform.SetParent(PovContrainer, false);
     }
 }
--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,21 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    // _ _ COMPONENTS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     public MovementComponent PlayerMovementComponent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    // _ _ TODO Hiding component _ _ _ _ _ _ _ _ _ _  _ _ _ _ _ 
+    [SerializeField] private SpriteRenderer Renderer;
+    [SerializeField] private CanvasRenderer FadeImage;
+    private bool _isHiding = false;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        FadeImage.SetAlpha(0.0f);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -23,5 +27,57 @@ public class PlayerController : MonoBehaviour
         Vector2 input = context.ReadValue<Vector2>();
         Vector3 movementDirection = new Vector3(input.x, .0f, input.y);
         PlayerMovementComponent.SetMovementDirection(movementDirection);
+    }
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            Hide();
+        }
+    }
+
+    //TODO : HidingComponent
+    private void Hide()
+    {
+        //TODO : disable player controller, switch to small camera controller? 
+        //TODO : Switch camera
+        _isHiding = !_isHiding;
+        RepositionCamera();
+    }
+
+    private async void RepositionCamera()
+    {
+        float alpha = 0.0f; 
+        while (alpha < 1.0f)
+        {
+            FadeImage.SetAlpha(alpha);
+            alpha += 0.05f;
+            await Task.Delay(1);
+        }
+        
+        SwitchCamera();
+        
+        await Task.Delay(10);
+        while (alpha >= 0.0f)
+        {
+            FadeImage.SetAlpha(alpha);
+            alpha -= 0.05f;
+            await Task.Delay(1);
+        }
+    }
+
+    private void SwitchCamera()
+    {
+        if (_isHiding)
+        {
+            Renderer.enabled = false;
+            CameraController.Instance.SetPovCamera();
+        }
+        else
+        {
+            Renderer.enabled = true;
+            CameraController.Instance.SetTopDownCamera();
+        }
     }
 }
