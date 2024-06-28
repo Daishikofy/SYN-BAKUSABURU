c21482f2aeaebdf0df84ea8e4c3d0e7beadf919e
Daishikofy <daishi.setsuko@gmail.com>
2024-06-28 17:38:57 +0200

    [GPP] Added health component

---
+++ Assets/Scripts/TOWER_Scripts/Components/TOW_HealthComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TOWER.Components
+{
+    [Serializable]
+    public class TOW_HealthComponent : MonoBehaviour
+    {
+        public int maxHealth = 10;
+        public int CurrentHealth { get; private set; }
+        
+        [Header("Callbacks")]
+        public UnityEvent onDefeated;
+
+        public TOW_HealthComponent()
+        {
+            CurrentHealth = maxHealth;
+        }
+
+        public void Damage(int damages)
+        {
+            if (IsAlive())
+            {
+                CurrentHealth -= damages;
+                if (CurrentHealth <= 0)
+                {
+                    onDefeated.Invoke();
+                }
+            }
+        }
+
+        public bool IsAlive()
+        {
+            return CurrentHealth > 0;
+        }
+    }
+}
