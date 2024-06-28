5aff6e173ca9fdff39042882fbacdcf487f301c6
Daishikofy <daishi.setsuko@gmail.com>
2024-06-28 17:50:18 +0200

    [GPP] Add base that can be damaged by enemies

---
+++ Assets/Scripts/TOWER_Scripts/TOW_BaseController.cs
@@ -0,0 +1,22 @@
+﻿using TOWER.Components;
+using UnityEngine;
+
+namespace TOWER
+{
+    [RequireComponent(typeof(TOW_HealthComponent))]
+    public class TOW_BaseController : MonoBehaviour
+    {
+        [SerializeField]
+        public TOW_HealthComponent healthComponent;
+        
+        private void Awake()
+        {
+            healthComponent.onDefeated.AddListener(OnDefeated);
+        }
+
+        private void OnDefeated()
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+}
