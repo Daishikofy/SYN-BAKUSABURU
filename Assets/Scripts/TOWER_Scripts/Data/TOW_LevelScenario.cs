52e0e876dd0761bce7a9ee471a4bc5a03a29c763
Daishikofy <daishi.setsuko@gmail.com>
2024-06-29 20:57:12 +0200

    [GPP] WIP : Enemy wave
    For now waves work with only 1 spawner

---
+++ Assets/Scripts/TOWER_Scripts/Data/TOW_LevelScenario.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TOWER
+{
+    [CreateAssetMenu]
+    public class TOW_LevelScenario : ScriptableObject
+    {
+        public TOW_EnemyWave[] enemyWaves = { };
+    }
+}
